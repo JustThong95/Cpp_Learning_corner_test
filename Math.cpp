5ac58f0f475036b9845a164726475bc62bac0e95
Justthong95 <thongsanji95@gmail.com>
2024-02-20 21:45:56 +0700

    Lmao

---
+++ Math.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ii pair<int, int>
+using namespace std;
+ll MOD = 1e9 + 7;
+
+int main()
+{
+    // ios_base::sync_with_stdio(NULL);
+    // cin.tie(0);
+    // cout.tie(0);
+    cout << 20 + 10;
+    return 0;
+}
